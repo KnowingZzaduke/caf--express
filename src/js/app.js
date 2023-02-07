5efb10133afcef98f89c65a2d0c8abbe2cca02c7
KnowingZzaduke <josex.developer@gmail.com>
2023-02-07 15:25:01 -0500

    Agregando dinamismo a las listas de los enlaces

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-//Mostrar menú
+//DISPOSITIVOS PEQUEÑOS
 const toggleAbrir = document.querySelector('.abrir');
 const toggleCerrar = document.querySelector('.cerrar');
 const menu = document.querySelector('.menu_none');
@@ -16,3 +16,61 @@ function cerrarMenu(){
         menu.classList.replace('menu', 'menu_none');
     }
 }
+
+const toggleEnlacesUp = document.querySelector('.toggle_enlaces-up');
+const toggleEnlacesDown = document.querySelector('.toggle_enlaces-down');
+const contentEnlaces = document.querySelector('.content_enlaces-hoy');
+
+//ABRIR LISTA ENLACES
+function abrirEnlaces(){
+    if(toggleEnlacesUp){
+        toggleEnlacesUp.style.display = "none";
+        toggleEnlacesDown.style.display = "block";
+        contentEnlaces.classList.add('enlaces_mostrar');
+    }
+}
+
+//CERRAR ENLACES
+function cerrarEnlaces(){
+    if(toggleEnlacesDown){
+        toggleEnlacesUp.style.display = "block";
+        toggleEnlacesDown.style.display = "none";
+        contentEnlaces.classList.remove('enlaces_mostrar');
+    }
+}
+
+let clientWidth = screen.width;
+function eliminarToggles(){
+    if(clientWidth >= 768){
+        toggleEnlacesUp.style.display = "none";
+        toggleEnlacesDown.style.display = "none";
+        contentEnlaces.classList.add('enlaces_mostrar-desktop')
+    }
+}
+let comprobarTamañoDesktop = setInterval(eliminarToggles, 0)
+
+
+//DISPOSITIVOS GRANDES
+const toggleCategoriasUp = document.querySelector('.toggle_categorias-up');
+const toggleCategoriasDown = document.querySelector('.toggle_categorias-down');
+const categorias = document.querySelector('.categorias');
+
+//MOSTRAR MENU
+function abrirMenuCategorias(){
+    if(toggleCategoriasUp){
+        categorias.classList.add('categorias_up');
+        toggleCategoriasUp.style.display = "none";
+        toggleCategoriasDown.style.display = "block";
+    }
+}
+
+//CERRAR MENÚ
+function cerrarMenuCategorias(){
+    if(toggleCategoriasDown){
+        categorias.classList.remove('categorias_up');
+        toggleCategoriasUp.style.display = "block";
+        toggleCategoriasDown.style.display = "none";
+    }
+}
+
+
